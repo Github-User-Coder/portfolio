2238c27e37ab493d936ddf82fa46f409167a9d8a
manoj yadav N R <manojmanu938055@gmail..com>
2024-12-06 19:19:55 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+       navToggle= document.getElementById('nav-toggle'),
+       navClose = document.getElementById('nav-close')
+
+// Menu Show  
+// validation if constant exists     
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+            //  menu hidden
+            // validate if constant exists
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}            
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink =document.querySelectorAll('.nav__link')
+const linkAction = ()=>{
+    const navMenu =document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader =()=>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
+
+/*=============== EMAIL JS ===============*/
+const contactForm=document.getElementById('contact-form'),
+      contactMessage=document.getElementById('contact-message')
+const sendEmail = (e)=>{
+  e.preventDefault()  
+  emailjs.sendForm('serviceId','template','#contact-form','publicID')
+  .then(()=>{
+    // show sent message
+    contactMessage.textContent='Message send successfully ✔'
+    // remove message after 5 sec
+    setTimeout(()=>{
+        contactMessage.textContent =''
+    },5000)
+
+    // clear input fields
+    contactForm.reset()
+  },()=>{
+    // show error message
+    contactMessage.textContent='Message not sent (service error)👎'
+  })
+}
+contactForm.addEventListener('submit',sendEmail)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp=()=>{
+    const scrollUp=document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewport Height, add show-scroll class to
+    this.scrollY >=350? scrollUp.classList.add('show-scroll')
+                   :scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive =()=>{
+     const scrollY =window.pageXOffset
+
+     sections.forEach(current =>{
+        const sectionHeight= current.offsetHeight,
+              sectionTop=current.offsetTop - 58,
+              sectionId=current.getAttribute('id'),
+              sectionsClass=document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+        if(scrollY> sectionTop && scrollY<= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+     })
+}
+window.addEventListener('scroll',scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    reset:true
+})
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin:'bottom'})
+sr.reveal(`.about__data, .skills__data`, {origin:'left'})
+sr.reveal(`.about__image, .skills__content`, {origin:'right'})
+sr.reveal(`.services__card, .projects__card`, {interval:100})
